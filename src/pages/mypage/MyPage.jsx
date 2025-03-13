c6fc997f17474361c5b5442018f2b9bd782f108f
callu9 <callu_9ine@naver.com>
2025-03-13 18:24:51 +0900

    rename: 폴더명 변경 (mypage copy > mypage)

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,10 +1,33 @@
 import Header from "../../components/header/Header";
+import IdolContextProvider, { useIdols, useSetIdols } from "../../contexts/IdolContext";
+import FavoriteIdols from "./components/FavoriteIdols";
+import IdolList from "./components/IdolList";
+import "./mypage.scss";
 
 export default function MyPage() {
 	return (
-		<>
+		<IdolContextProvider>
 			<Header />
-			<main className="display-grid justify-center my-76"></main>
-		</>
+			<MyPageContainer />
+		</IdolContextProvider>
+	);
+}
+function MyPageContainer() {
+	const idols = useIdols();
+	const setIdols = useSetIdols();
+
+	function handleRemove(id) {
+		setIdols(idols.filter((idol) => idol.id !== id));
+	}
+	function handleAdd(list) {
+		setIdols([...idols, ...list]);
+	}
+
+	return (
+		<main className="display-grid justify-stretch gap-40 mx-auto my-76" id="my-page">
+			<FavoriteIdols idols={idols} onRemove={handleRemove} />
+			<hr />
+			<IdolList favoriteIdols={idols} onAdd={handleAdd} />
+		</main>
 	);
 }
