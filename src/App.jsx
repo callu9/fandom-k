8f0faeedc3b197d072f545ec4c922db0aebff033
callu9 <callu_9ine@naver.com>
2025-03-19 16:18:45 +0900

    feat/ optional: 추가 기능 개발 (아이돌 프로필 등록, 조공 생성)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import GlobalContextProvider from "./contexts/GlobalContext";
+import AddIdol from "./pages/addidol/AddIdol";
+import AddTribute from "./pages/addtribute/AddTribute";
 import LandingPage from "./pages/LandingPage";
 import ListPage from "./pages/list/ListPage";
 import MyPage from "./pages/mypage/MyPage";
-import GlobalContextProvider from "./contexts/GlobalContext";
+import NotFound from "./pages/notfound/NotFound";
+
+const router = createBrowserRouter([
+	{ path: "/", element: <LandingPage /> },
+	{ path: "/list", element: <ListPage /> },
+	{ path: "/mypage", element: <MyPage /> },
+	{ path: "/add_idol", element: <AddIdol /> },
+	{ path: "/add_tribute", element: <AddTribute /> },
+	{ path: "*", element: <NotFound /> },
+]);
 
 function App() {
 	return (
 		<GlobalContextProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/list" element={<ListPage />} />
-					<Route path="/mypage" element={<MyPage />} />
-				</Routes>
-			</BrowserRouter>
+			<RouterProvider router={router} />
 		</GlobalContextProvider>
 	);
 }
